895e4f01b36f83c2bbdc6d2682b9285e59b2957d
RonaldVilorio <ronvilorio@gmail.com>
2021-03-13 18:37:19 -0500

    logic for winner added

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const randomNum1 = Math.floor(Math.random() * (7 - 1) + 1);
+const randomNum2 = Math.floor(Math.random() * (7 - 1) + 1);
+
 document.querySelector(".img1").src = `images/dice${randomNum1}.png`;
+document.querySelector(".img2").src = `images/dice${randomNum2}.png`;
+
+if (randomNum1 > randomNum2) {
+  document.querySelector("h1").innerText = "Player 1 Wins!";
+} else if (randomNum2 > randomNum1) {
+  document.querySelector("h1").innerText = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerText = "Draw!";
+}
